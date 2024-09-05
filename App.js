639325b348b20b5076d7c6c73de95daaeb05d463
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-09-05 16:32:37 +0200

    Add the main icons

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Entypo from "@expo/vector-icons/Entypo";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Entypo name="pencil" size={24} color="#eee" />
+      <AntDesign name="smile-circle" size={24} color="#f7c" />
+      <AntDesign name="circledown" size={24} color="#38c" />
     </View>
   );
 }
@@ -13,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "black",
     alignItems: "center",
     justifyContent: "center",
   },
