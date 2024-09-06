98dffa4e6420504baefd3ca2173ba3d81f14f82b
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-09-06 15:54:16 +0200

    Add the game logic

--- App.js
+++ App.js
@@ -1,16 +1,121 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import Entypo from "@expo/vector-icons/Entypo";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import { Snackbar } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
+  const [isCross, setIsCross] = useState(false);
+  const [gameWinner, setGameWinner] = useState("");
+  const [gameState, setGameState] = useState(new Array(9).fill("empty", 0, 9));
+  const [snackVisible, setSnackVisible] = useState(false);
+  const [snackMessage, setSnackMessage] = useState("");
+
+  const reloadGame = () => {
+    setIsCross(false);
+    setGameWinner("");
+    setGameState(new Array(9).fill("empty", 0, 9));
+  };
+
+  const checkIsWinner = () => {
+    if (
+      gameState[0] === gameState[1] &&
+      gameState[0] === gameState[2] &&
+      gameState[0] !== "empty"
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[3] !== "empty" &&
+      gameState[3] === gameState[4] &&
+      gameState[4] === gameState[5]
+    ) {
+      setGameWinner(`${gameState[3]} won the game! 🥳`);
+    } else if (
+      gameState[6] !== "empty" &&
+      gameState[6] === gameState[7] &&
+      gameState[7] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[6]} won the game! 🥳`);
+    } else if (
+      gameState[0] !== "empty" &&
+      gameState[0] === gameState[3] &&
+      gameState[3] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[1] !== "empty" &&
+      gameState[1] === gameState[4] &&
+      gameState[4] === gameState[7]
+    ) {
+      setGameWinner(`${gameState[1]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== "empty" &&
+      gameState[2] === gameState[5] &&
+      gameState[5] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (
+      gameState[0] !== "empty" &&
+      gameState[0] === gameState[4] &&
+      gameState[4] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== "empty" &&
+      gameState[2] === gameState[4] &&
+      gameState[4] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (!gameState.includes("empty", 0)) {
+      setGameWinner("Draw game... ⌛️");
+    }
+  };
+
+  const onChangeItem = (itemNumber) => {
+    if (gameWinner) {
+      setSnackMessage(gameWinner);
+      setSnackVisible(true);
+      return;
+    }
+
+    if (gameState[itemNumber] === "empty") {
+      gameState[itemNumber] = isCross ? "cross" : "circle";
+      setIsCross(!isCross);
+    } else {
+      setSnackMessage("Position is already filled");
+      setSnackVisible(true);
+    }
+
+    checkIsWinner();
+  };
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <Entypo name="pencil" size={24} color="#eee" />
-      <AntDesign name="smile-circle" size={24} color="#f7c" />
-      <AntDesign name="circledown" size={24} color="#38c" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="light" />
+        <View style={styles.container}>
+          <Text style={styles.title}>Tic Tac Toe</Text>
+          <Entypo name="pencil" size={24} color="#eee" />
+          <AntDesign name="smile-circle" size={24} color="#f7c" />
+          <AntDesign name="circledown" size={24} color="#38c" />
+        </View>
+
+        <Snackbar
+          visible={snackVisible}
+          onDismiss={() => setSnackVisible(false)}
+          action={{
+            label: "OK",
+            onPress: () => {
+              setSnackVisible(false);
+            },
+          }}
+        >
+          {snackMessage}
+        </Snackbar>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -20,5 +125,10 @@ const styles = StyleSheet.create({
     backgroundColor: "black",
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    fontSize: 32,
+    color: "#fff",
+    marginBottom: 16,
   },
 });
